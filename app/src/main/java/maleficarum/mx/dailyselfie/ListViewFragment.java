49b98115eae8373753af94d2ba433208add61dc7
Oscar <o@maleficarum.mx>
2014-11-26 15:45:25 -0600

    First skeleton

---
+++ app/src/main/java/maleficarum/mx/dailyselfie/ListViewFragment.java
@@ -0,0 +1,48 @@
+package maleficarum.mx.dailyselfie;
+
+import android.app.ListFragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by oscar on 11/26/14.
+ */
+public class ListViewFragment extends ListFragment {
+    private List<ListViewItem> mItems;        // ListView items list
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // initialize the items list
+        mItems = new ArrayList<ListViewItem>();
+        Resources resources = getResources();
+
+        mItems.add(new ListViewItem(null, "TEST", "TEST"));
+
+        // initialize and set the list adapter
+        setListAdapter(new ListViewAdapter(getActivity(), mItems));
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // remove the dividers from the ListView of the ListFragment
+        getListView().setDivider(null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // retrieve theListView item
+        ListViewItem item = mItems.get(position);
+
+        // do something
+        Toast.makeText(getActivity(), item.title, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/maleficarum/mx/dailyselfie/MainActivity.java
+++ app/src/main/java/maleficarum/mx/dailyselfie/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListViewFragment fragment = new ListViewFragment();
+        getFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
     }
 
 
