7811c869270f8b16c869691507a052f1cec14063
Oscar <o@maleficarum.mx>
2014-11-26 16:25:00 -0600

    Added new scheduling

--- app/src/main/java/maleficarum/mx/dailyselfie/MainActivity.java
+++ app/src/main/java/maleficarum/mx/dailyselfie/MainActivity.java
@@ -1,6 +1,8 @@
 package maleficarum.mx.dailyselfie;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -29,6 +32,26 @@ public class MainActivity extends Activity {
 
         fragment = new ListViewFragment();
         getFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
+
+        //
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.MONTH, 6);
+        calendar.set(Calendar.YEAR, 2013);
+        calendar.set(Calendar.DAY_OF_MONTH, 13);
+
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 48);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.AM_PM, Calendar.PM);
+
+        Intent myIntent = new Intent(MainActivity.this, MyReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+
+        //
     }
 
 
